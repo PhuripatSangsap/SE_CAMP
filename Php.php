8b86bd838abc8728aa61e6585c789dfa4902f4fd
PhuripatSangsap <152171578+PhuripatSangsap@users.noreply.github.com>
2023-12-25 19:38:06 +0700

    New php

---
+++ Php.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="utf-8"> 
+    </head>
+    <body>
+        <h1><?php echo "Hello world<br><br>"; ?>></h1>
+        <?php echo "<h1>Hello World<br><br></h1>" ?>
+        <?php $var_a = "" ?>
+        <?php $var_b = '' ?>
+        <?php $var_c = "" ?>
+    </body>
+</html>
