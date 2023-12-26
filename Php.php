f07096a8dbc91817397dd49a8c3ed069a4893599
PhuripatSangsap <152171578+PhuripatSangsap@users.noreply.github.com>
2023-12-26 11:26:06 +0700

    Up php

--- Php.php
+++ Php.php
@@ -1,12 +1,71 @@
 <html>
-    <head>
-        <meta charset="utf-8"> 
-    </head>
-    <body>
-        <h1><?php echo "Hello world<br><br>"; ?>></h1>
-        <?php echo "<h1>Hello World<br><br></h1>" ?>
-        <?php $var_a = "" ?>
-        <?php $var_b = '' ?>
-        <?php $var_c = "" ?>
-    </body>
+
+<head>
+
+    <meta charset="utf-8">
+
+</head>
+
+<body>
+
+    <div class="container">
+        <?php $multi_x = 24; ?>
+
+        <h1> ตารางสูตรคูณแม่ <?php echo $multi_x ?> </h1>
+
+        <div class="multiply_x">
+            <?php
+
+            for ($i = 1; $i <= 24; $i++) {
+
+                echo "{$multi_x} x {$i} = " . ($multi_x * $i) . "<br/>" . "<br/>";
+            }
+
+            ?>
+        </div>
+    </div>
+
+    <style>
+        .container {
+
+            display: flex;
+            width: 100%;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+
+        }
+
+        body {
+
+            display: flex;
+            height: 90vh;
+            justify-content: center;
+            background-color: black;
+
+        }
+
+        h1 {
+
+            text-transform: uppercase;
+            font-size: 50px;
+            color: red;
+
+        }
+
+        .multiply_x {
+
+            width: 200px;
+            height: calc(100vh - 15rem);
+            overflow-y: scroll;
+
+            color: white;
+            font-size: 1.5rem;
+            text-align: center;
+
+        }
+
+    </style>
+</body>
+
 </html>
